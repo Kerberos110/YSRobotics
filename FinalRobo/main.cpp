e9253057d1e355cd50cf298e6578a075882da125
Student Name <student@gmail.com>
2015-07-15 21:16:15 +0300

    Creating new project

---
+++ FinalRobo/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Kerberos
+ *
+ * Created on July 15, 2015, 9:14 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
