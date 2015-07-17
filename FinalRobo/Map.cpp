eb42bbf0865d2d13b1cabfd1b65a1166d9ea0149
Student Name <student@gmail.com>
2015-07-17 12:52:26 +0300

    map is now working, need to init the CFG as singleton

--- FinalRobo/Map.cpp
+++ FinalRobo/Map.cpp
@@ -2,6 +2,7 @@
 #include "Map.h"
 #include <iostream>
 #include "CfgMgr.h"
+#include <stdio.h>
 using namespace std;
 
 const int COLOR_WHITE=0;
@@ -19,6 +20,8 @@ void LoadMap(const char* filename)
   unsigned error = lodepng::decode(image, width, height, png);  
   if(error) std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
   
+  
+  std::vector< std::vector<int> > map_Array(height,std::vector<int>(width));
   int map[width][height];
   int bytesIndex=0;
   while(bytesIndex<image.size()-3)
@@ -29,21 +32,27 @@ void LoadMap(const char* filename)
       int A = image[bytesIndex+3];
       
       int color = A+R+G+B;
-      map[(bytesIndex/4)%width][(bytesIndex/4)/width] = color;
-      /*if(color==255)
+      //map[(bytesIndex/4)%width][(bytesIndex/4)/width] = color;
+      //map_Array[(bytesIndex/4)%width][(bytesIndex/4)/width] = color;
+      if(color==255)
       {
         map[(bytesIndex/4)%width][(bytesIndex/4)/width] = COLOR_BLACK;
-        cout << "[" << (bytesIndex/4)%width << "," << (bytesIndex/4)/width<< "]\n";
-      }*/
+      }
+      else if(color=1020)
+      {
+        map[(bytesIndex/4)%width][(bytesIndex/4)/width] = COLOR_WHITE;
+      } 
       bytesIndex=bytesIndex+4;
+  }   
       
       //double DIVFACTOR = (double((double)CFG_GRID_RES/CFG_MAP_RES));
     int DIVFACTOR = 4;
-    int DIVIFACTOR = 4;
-    int grid[width/DIVIFACTOR][height/DIVIFACTOR];
-
-    for (int iwidth = 0; iwidth < width; iwidth=iwidth+DIVFACTOR) {
-      for (int iheight = 0; iheight < height; iheight+DIVFACTOR) {
+    int grid[width/DIVFACTOR][height/DIVFACTOR];
+    int iwidth;
+    int iheight;
+    
+    for (iwidth = 0; iwidth < width; iwidth=iwidth+DIVFACTOR) {
+      for (iheight = 0; iheight < height; iheight=iheight+DIVFACTOR) {
 
           bool bIsDIrty = false;   
           for (int xrunner = 0; xrunner < DIVFACTOR; xrunner++) {
@@ -56,18 +65,37 @@ void LoadMap(const char* filename)
              }
           if(!bIsDIrty)
           {
-              grid[iwidth/DIVIFACTOR][iheight/DIVIFACTOR] = COLOR_WHITE;
+              grid[iwidth/DIVFACTOR][iheight/DIVFACTOR] = COLOR_WHITE;
           }
           else
           {
-              grid[iwidth/DIVIFACTOR][iheight/DIVIFACTOR] = COLOR_BLACK;
+              grid[iwidth/DIVFACTOR][iheight/DIVFACTOR] = COLOR_BLACK;
           }
-          iheight +=DIVIFACTOR;
+          iheight +=DIVFACTOR;
       }
-      iwidth +=DIVIFACTOR;
+      iwidth +=DIVFACTOR;
+  }
+    
+    CfgMgr cmg= CfgMgr();
+    
+    grid[cmg.CFG_ROBOINIT_LOCATIONX][cmg.CFG_ROBOINIT_LOCATIONY]=2;
 
-  }
-  }
+    
+        for(int y=0;y<iheight/4;y++)
+        {
+            for(int x=0;x<iwidth/4;x++)
+                if(grid[x][y]==COLOR_WHITE)
+                    cout<<".";
+                else if(grid[x][y]==COLOR_BLACK)
+                    cout<<"O"; //obstacle
+                else if(grid[x][y]==2)
+                    cout<<"S"; //start
+                else if(grid[x][y]==3)
+                    cout<<"R"; //route
+                else if(grid[x][y]==4)
+                    cout<<"F"; //finish
+            cout<<endl;
+        }
   
 }
 
